9d987a188dc23d078a01d41c1f5bc012ce7f361f
Prakriti Pokharel <iprakritip@gmail.com>
2023-09-01 09:34:04 +0545

    chore : use assertEqual instead of assertEquals

--- shortener/tests.py
+++ shortener/tests.py
@@ -1,9 +1,10 @@
+import json
 import string
-from django.test import TestCase
+from django.test import TestCase, Client
 from shortener.models import URL_Table
+from django.urls import reverse, resolve
 from django.contrib.auth.models import User
 from shortener.short import generate_unique_id
-from django.urls import reverse, resolve
 from shortener.views import shortener,redirect
 
 class URLTableTest(TestCase):
@@ -11,7 +12,7 @@ class URLTableTest(TestCase):
     def test_URL_Table(self):
         user = User.objects.create_user(username='testuser', password='testpassword')
         url = URL_Table.objects.create(original_url="www.spotify.com", user=user)
-        self.assertEquals(str(url), 'www.spotify.com')
+        self.assertEqual(str(url), 'www.spotify.com')
         print("IsAUrl : ",isinstance(url,URL_Table))
         self.assertTrue(isinstance(url,URL_Table))
 
@@ -20,12 +21,12 @@ class shorteningLogicTest(TestCase):
     
     def test_id_default_length(self):
         unique_id=generate_unique_id()
-        self.assertEquals(len(unique_id),5)
+        self.assertEqual(len(unique_id),5)
         self.assertTrue(all(x in string.ascii_letters + string.digits for x in unique_id))
 
     def test_id_custom_length(self):
         unique_id=generate_unique_id(8)
-        self.assertEquals(len(unique_id),8)
+        self.assertEqual(len(unique_id),8)
         self.assertTrue(all(x in string.ascii_letters + string.digits for x in unique_id))
 
 
@@ -33,8 +34,26 @@ class urlPatternTest(TestCase):
 
     def test_shotener_url(self):
         url=reverse('shortener')
-        self.assertEquals(resolve(url).func,shortener)
+        self.assertEqual(resolve(url).func,shortener)
 
     def test_redirect_url(self):
         url=reverse('redirect', args=['SplYo'])
-        self.assertEquals(resolve(url).func,redirect)
+        self.assertEqual(resolve(url).func,redirect)
+
+
+class shortenerViewTest(TestCase):
+
+    def setUp(self):
+        self.client=Client()
+        self.user=User.objects.create_user(username='testuser', password='testpassword')
+        self.url="/shortener/"
+
+    # def test_shorterner_post_success(self):
+    #     data = {
+    #         "original_url": "www.facebook.com",
+    #         "user_id": self.user.id
+    #     }
+    #     response=self.client.post(self.url,json.dumps(data),content_type='application/json')
+
+    #     # self.assertEquals(response.status_code,200)
+    #     self.assertEqual(response.json,{"link-status":"received"})
