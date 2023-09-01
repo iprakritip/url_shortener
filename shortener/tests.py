d36167b05bc5af1411cd29dc394f9f823bb85052
Prakriti Pokharel <iprakritip@gmail.com>
2023-09-01 07:43:42 +0545

    chore : write test for URL_Table model

--- shortener/tests.py
+++ shortener/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from shortener.models import URL_Table
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class URLTableTest(TestCase):
+    
+    def test_URL_Table(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        url = URL_Table.objects.create(original_url="www.spotify.com", user=user)
+        self.assertEquals(str(url), 'www.spotify.com')
+        print("IsAUrl : ",isinstance(url,URL_Table))
+        self.assertTrue(isinstance(url,URL_Table))
